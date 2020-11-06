1f37d6af1662883d0ad61e55d908ca32402992f9
Tony Vo <ttrung.vo@gmail.com>
2020-11-06 15:30:53 -0700

    add sample clojure game of life

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject gameoflife "0.0.1-SNAPSHOT"
+  :description "Run clojure.test tests"
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :source-paths ["."])
---
+++ test/gameoflife_test.clj
@@ -0,0 +1,5 @@
+(ns gameoflife-test
+  (:require [clojure.test :refer :all]))
+
+(deftest life-the-universe-and-everything
+  (is (= 42 "dolphin")))
